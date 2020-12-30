d3c2d47e8e9326a18e27cbd17bbb405945ab98f3
rvibek <rvibek@gmail.com>
2020-12-30 11:17:20 +0200

    init

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,7 +1,9 @@
 // paste in your published Google Sheets URL from the browser address bar
 
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+// var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
 
 // var googleDocURL = 'https://docs.google.com/spreadsheets/d/1pVRRpKcR8cnwhjW7-NH9raOo2RAv_aqvP-10-VNEVuU/edit#gid=0';
+
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1pVRRpKcR8cnwhjW7-NH9raOo2RAv_aqvP-10-VNEVuU/edit#gid=0';
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
